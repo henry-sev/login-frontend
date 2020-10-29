5a61f14bfb8e55c0de785af1b780fb4ebeba5ee5
henry-sev <q2466351926@gmail.com>
2020-10-29 23:54:57 +0800

    log in with account

---
+++ index.js
@@ -0,0 +1,60 @@
+// window.onload = function() {
+//   fetch('http://127.0.0.1:3000')
+//     .then(res => {
+//       // console.log(res);
+//       return res.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//     })
+// }
+
+
+function getName() {
+  const userName = document.querySelector('#user-name');
+  return userName.value.trim();
+}
+
+function getPassword() {
+  const userPassward = document.querySelector('#user-password');
+  return userPassward.value.trim();
+}
+
+function checkAccount() {
+  let $name = getName();
+  let $password = getPassword();
+  let $result = false;
+  fetch('http://127.0.0.1:3000')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      data.forEach(item => {
+        if (item['name'] === $name && item['password'] === $password) {
+          $result = true;
+        } 
+      });
+      if ($result) window.location.href="https://www.hackerstart.cn/";
+      else {
+        alert('账户不存在，请注册')
+        window.location.href="./sign-up.html"
+      };
+    })
+}
+
+// async function logIn() {
+//   if (await checkAccount()) {
+//     console.log('账户存在，已登录');
+//   }
+//   else {
+//     console.log('账户不存在，请注册');
+//   }
+// }
+
+function goToSignUp() {
+  const loginBtn = document.querySelector('#login-btn');
+  let signUp = document.createElement('a');
+  signUp.setAttribute('href', './sign-up.html');
+  signUp.textContent = 'Log in';
+}
+
